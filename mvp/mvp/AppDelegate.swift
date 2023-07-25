446e5a42ad4c7e9c4bbc0962accb9bf2f2ccb658
dmitrycvetkov2000 <dimacvetkov2000@mail.ru>
2023-07-25 03:11:57 +0300

    setup proj

---
+++ mvp/mvp/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  mvp
+//
+//  Created by Дмитрий Цветков on 25.07.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow()
+        let vc = ViewController()
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+}
+
---
+++ mvp/mvp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvp
+//
+//  Created by Дмитрий Цветков on 25.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
